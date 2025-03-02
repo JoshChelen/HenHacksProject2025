98ab247f1285ad9cfe8a5020b7e1409940a4fc6d
JiaQi312 <148893867+JiaQi312@users.noreply.github.com>
2025-03-01 21:12:22 -0500

    Added comments to save button script

--- Assets/Scripts/SaveButtonScript.cs
+++ Assets/Scripts/SaveButtonScript.cs
@@ -4,9 +4,11 @@ using TMPro;
 
 public class SaveButtonScript : MonoBehaviour
 {
-
+    //field to hold our current notepad string
     public string theText;
+    //notepad object
     public TMP_InputField ourNote;
+    //object to load our saved string into
     public TMP_InputField notePad;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
